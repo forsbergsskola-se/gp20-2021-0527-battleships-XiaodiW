4a61a051cf3a6f902f2747490d901eeeda1145c6
Xiaodi Wang <xiaodi@kth.se>
2021-05-31 19:09:34 +0200

    Implement Fibonacci Recursion way

--- FibonacciNumbers/main.cpp
+++ FibonacciNumbers/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-
+using namespace std;
+int Fibonacci(int n){
+    if ( ( n == 0 ) || ( n == 1 ) )
+        return n;
+    else
+        return Fibonacci( n - 1 ) + Fibonacci( n - 2 );
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    cout << "Which Fibonacci Numbers do you want?";
+    int n;
+    cin >> n;
+    cout << "Fibonacci(" << n << ") = " << Fibonacci(n);
     return 0;
 }
