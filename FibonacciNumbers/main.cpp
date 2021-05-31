3cbd153be538e5ef53a22eb440c5edd979c51b36
Xiaodi Wang <xiaodi@kth.se>
2021-05-31 19:22:56 +0200

    Implement Fibonacci Iteration way

--- FibonacciNumbers/main.cpp
+++ FibonacciNumbers/main.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
 using namespace std;
-int Fibonacci(int n){
+int FibonacciRecursion(int n){
     if ( ( n == 0 ) || ( n == 1 ) )
         return n;
     else
-        return Fibonacci( n - 1 ) + Fibonacci( n - 2 );
+        return FibonacciRecursion(n - 1) + FibonacciRecursion(n - 2);
+}
+
+int FibonacciIteration(int n){
+    static int a = 0, b = 1, c;
+    if ( ( n == 0 ) || ( n == 1 ) )
+        return n;
+    else
+        for (int i = 1; i < n; ++i) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    return c;
 }
 int main() {
     cout << "Hello, World!" << endl;
-    cout << "Which Fibonacci Numbers do you want?";
+    cout << "Which FibonacciRecursion Numbers do you want?";
     int n;
     cin >> n;
-    cout << "Fibonacci(" << n << ") = " << Fibonacci(n);
+    cout << "Recursion FibonacciRecursion(" << n << ") = " << FibonacciRecursion(n) << endl;
+    cout << "Iteration FibonacciRecursion(" << n << ") = " << FibonacciIteration(n) << endl;
+
     return 0;
 }
