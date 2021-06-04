6348e4a8794784b8521f2120b0fa9818e3806d71
Xiaodi Wang <xiaodi@kth.se>
2021-06-04 10:31:16 +0200

    Implement Random Number

--- RandomNumbers/main.cpp
+++ RandomNumbers/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    const int size = 20;
+    int frequency[size] = {0};
+    int max = 1000000;
+    for (int i = 0; i < max; ++i) {
+        int number = rand() % size;
+        frequency[number] ++;
+    }
+    float percent[size] = {0.0f};
+    float offset[size] = {0.0f};
+    float expectFrequency = 1.0f / 20 * 100;
+    float offsetSum = 0.0f;
+    for (int i = 0; i < size; ++i) {
+        percent[i] = static_cast<float>(frequency[i]) / max * 100;
+        offset[i] = abs(percent[i] - expectFrequency);
+        offsetSum += offset[i];
+        cout << "Number " << i << "  " << frequency[i] << " times" << " (" << percent[i] << "%)" << endl;
+    }
+    int maxIndex = distance(offset,max_element(offset,offset + size));
+    cout << "Maximum Offset: Number " << maxIndex << " (" << offset[maxIndex] << "%)" << endl;
+    int minIndex = distance(offset,min_element(offset,offset + size));
+    cout << "Minimum Offset: Number " << minIndex << " (" << offset[minIndex] << "%)" << endl;
+    cout << "Average Offset "<< offsetSum / size << "%" << endl;
+
     return 0;
 }
